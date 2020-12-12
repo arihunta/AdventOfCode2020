dd4b93dba61a811c6fe8bad92150848cadd23c37
Shitesh Arihunta <arihunta@gmail.com>
2020-12-12 23:39:54 +0200

    Day 11-02

--- lib/day_11.rb
+++ lib/day_11.rb
@@ -1,10 +1,14 @@
 module Arihunta
   module AdventOfCode2020
     class Day11
+      Directions = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
+
+      @@sight_lines_cache = Hash.new
+
       def self._01(data)
-        seats = File.readlines(data).map { |line| ['.', line.strip.chars, '.'].flatten }
-        seats.push( Array.new(seats[0].size) { '.'} )
-        seats.unshift( Array.new(seats[0].size) { '.'} )
+        seats = File.readlines(data).map { |line| [".", line.strip.chars, "."].flatten }
+        seats.push(Array.new(seats[0].size) { "." })
+        seats.unshift(Array.new(seats[0].size) { "." })
         has_changed = true
         seat_count = 0
         while has_changed
@@ -13,14 +17,14 @@ module Arihunta
           for i in 1..(seats.size - 2)
             for j in 1..(seats[i].size - 2)
               if seats[i][j] == "#"
-                if neighbours([i, j]).reject { |coord| seats[coord[0]][coord[1]] != '#' }.size >= 4
-                  tmp[i][j] = 'L'
+                if neighbours([i, j]).reject { |coord| seats[coord[0]][coord[1]] != "#" }.size >= 4
+                  tmp[i][j] = "L"
                   has_changed = true
                   seat_count = seat_count - 1
                 end
               elsif seats[i][j] == "L"
-                if !neighbours([i, j]).any? { |coord| seats[coord[0]][coord[1]] == '#' }
-                  tmp[i][j] = '#'
+                if !neighbours([i, j]).any? { |coord| seats[coord[0]][coord[1]] == "#" }
+                  tmp[i][j] = "#"
                   has_changed = true
                   seat_count = seat_count + 1
                 end
@@ -33,6 +37,35 @@ module Arihunta
       end
 
       def self._02(data)
+        @@sight_lines_cache.clear
+        seats = File.readlines(data).map { |line| [" ", line.strip.chars, " "].flatten }
+        seats.push(Array.new(seats[0].size) { " " })
+        seats.unshift(Array.new(seats[0].size) { " " })
+        has_changed = true
+        seat_count = 0
+        while has_changed
+          has_changed = false
+          tmp = Marshal.load(Marshal.dump(seats))
+          for i in 1..(seats.size - 2)
+            for j in 1..(seats[i].size - 2)
+              if seats[i][j] == "#"
+                if sight_lines([i, j], seats).reject { |coord| seats[coord[0]][coord[1]] != "#" }.size >= 5
+                  tmp[i][j] = "L"
+                  has_changed = true
+                  seat_count = seat_count - 1
+                end
+              elsif seats[i][j] == "L"
+                if !sight_lines([i, j], seats).any? { |coord| seats[coord[0]][coord[1]] == "#" }
+                  tmp[i][j] = "#"
+                  has_changed = true
+                  seat_count = seat_count + 1
+                end
+              end
+            end
+          end
+          seats = tmp
+        end
+        seat_count
       end
 
       def self.printseats(seats)
@@ -52,6 +85,24 @@ module Arihunta
           [coord[0], coord[1] + 1],
           [coord[0] + 1, coord[1] + 1],
         ]
+      end
+
+      def self.sight_lines(coords, grid)
+        if @@sight_lines_cache.has_key?(coords)
+          return @@sight_lines_cache[coords]
+        end
+        sight_lines = Directions.map { |direction|
+          neighbour = add_vectors(coords, direction)
+          while grid[neighbour[0]][neighbour[1]] == "."
+            neighbour = add_vectors(neighbour, direction)
+          end
+          grid[neighbour[0]][neighbour[1]] == " " ? nil : neighbour
+        }.reject { |neighbour| neighbour == nil }
+        @@sight_lines_cache[coords] = sight_lines
+      end
+
+      def self.add_vectors(a, b)
+        [a[0] + b[0], a[1] + b[1]]
       end
     end
   end
--- spec/day_11_spec.rb
+++ spec/day_11_spec.rb
@@ -11,11 +11,11 @@ describe Day11 do
     expect(Day11._01("data/11")).to eql(2211)
   end
 
-  # it "02-00" do
-  #   expect(Day11._02("data/11-00")).to eql(8)
-  # end
+  it "02-00" do
+    expect(Day11._02("data/11-00")).to eql(26)
+  end
 
-  # it "02" do
-  #   expect(Day11._02("data/11")).to eql(12401793332096)
-  # end
+  it "02" do
+    expect(Day11._02("data/11")).to eql(1995)
+  end
 end
