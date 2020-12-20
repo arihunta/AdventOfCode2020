989f9f972e1242782cea73e1fbfbc09b817a8a58
Shitesh Arihunta <arihunta@gmail.com>
2020-12-20 02:22:49 +0200

    Day 15

--- lib/day_15.rb
+++ lib/day_15.rb
@@ -2,9 +2,45 @@ module Arihunta
   module AdventOfCode2020
     class Day15
       def self._01(data)
+        history = Hash.new
+        numbers = File.read(data).strip.split(',')
+          .map { |x| x.to_i }
+        turn = numbers.size + 1
+        lastnum = numbers.last
+        numbers.pop
+        numbers.each_with_index { |x, i| history[x] = i + 1 }
+        while turn <= 2020 do
+          if history.has_key?(lastnum)
+            nextnum = turn - history[lastnum] - 1
+          else
+            nextnum = 0
+          end
+          history[lastnum] = turn - 1
+          lastnum = nextnum
+          turn += 1
+        end
+        lastnum
       end
 
       def self._02(data)
+        history = Hash.new
+        numbers = File.read(data).strip.split(',')
+          .map { |x| x.to_i }
+        turn = numbers.size + 1
+        lastnum = numbers.last
+        numbers.pop
+        numbers.each_with_index { |x, i| history[x] = i + 1 }
+        while turn <= 30000000 do
+          if history.has_key?(lastnum)
+            nextnum = turn - history[lastnum] - 1
+          else
+            nextnum = 0
+          end
+          history[lastnum] = turn - 1
+          lastnum = nextnum
+          turn += 1
+        end
+        lastnum
       end
     end
   end
--- spec/day_15_spec.rb
+++ spec/day_15_spec.rb
@@ -3,19 +3,19 @@ require "day_15"
 Day15 = Arihunta::AdventOfCode2020::Day15
 
 describe Day15 do
-  # it "01-00" do
-  #   expect(Day15._01("data/15-00")).to eql(295)
-  # end
+  it "01-00" do
+    expect(Day15._01("data/15-00")).to eql(436)
+  end
 
-  # it "01" do
-  #   expect(Day15._01("data/15")).to eql(102)
-  # end
+  it "01" do
+    expect(Day15._01("data/15")).to eql(1111)
+  end
 
-  # it "02-00" do
-  #   expect(Day15._02("data/15-00")).to eql(1068781)
-  # end
+  it "02-00" do
+    expect(Day15._02("data/15-00")).to eql(175594)
+  end
 
-  # it "02" do
-  #   expect(Day15._02("data/15")).to eql(42015)
-  # end
+  it "02" do
+    expect(Day15._02("data/15")).to eql(48568)
+  end
 end
