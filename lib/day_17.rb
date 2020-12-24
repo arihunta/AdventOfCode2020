110510850e9c5d523050205ac00cc3a33b0f79c1
Shitesh Arihunta <arihunta@gmail.com>
2020-12-24 20:10:36 +0200

    Day 17

--- lib/day_17.rb
+++ lib/day_17.rb
@@ -1,10 +1,100 @@
+require "set"
+
 module Arihunta
   module AdventOfCode2020
     class Day17
+      NeighbourDeltas = [[-1, -1, -1],
+              [-1, -1, 0],
+              [-1, -1, 1],
+              [-1, 0, -1],
+              [-1, 0, 0],
+              [-1, 0, 1],
+              [-1, 1, -1],
+              [-1, 1, 0],
+              [-1, 1, 1],
+              [0, -1, -1],
+              [0, -1, 0],
+              [0, -1, 1],
+              [0, 0, -1],
+              [0, 0, 1],
+              [0, 1, -1],
+              [0, 1, 0],
+              [0, 1, 1],
+              [1, -1, -1],
+              [1, -1, 0],
+              [1, -1, 1],
+              [1, 0, -1],
+              [1, 0, 0],
+              [1, 0, 1],
+              [1, 1, -1],
+              [1, 1, 0],
+              [1, 1, 1]]
+
       def self._01(data)
+        active_cubes = Set.new
+        File.readlines(data).each_with_index { |line, y|
+          line.strip.chars.each_with_index { |char, x|
+            active_cubes << [x, y, 0] if char == "#"
+          }
+        }
+
+        for i in 1..6
+          new_active = Set.new
+          active_cubes.map { |coord| neighbours(coord) + [coord] }
+            .flatten(1)
+            .uniq
+            .each { |candidate|
+              num_active_neighbours = neighbours(candidate).select{ |neighbour| active_cubes.include?(neighbour) }.size
+              if active_cubes.include?(candidate)
+                new_active << candidate if num_active_neighbours == 2 || num_active_neighbours == 3 
+              else
+                new_active << candidate if num_active_neighbours == 3
+              end
+            }
+          active_cubes = new_active
+        end
+
+        active_cubes.size
       end
 
       def self._02(data)
+        active_cubes = Set.new
+        File.readlines(data).each_with_index { |line, y|
+          line.strip.chars.each_with_index { |char, x|
+            active_cubes << [x, y, 0, 0] if char == "#"
+          }
+        }
+
+        for i in 1..6
+          new_active = Set.new
+          active_cubes.map { |coord| hypercube_neighbours(coord) + [coord] }
+            .flatten(1)
+            .uniq
+            .each { |candidate|
+              num_active_neighbours = hypercube_neighbours(candidate).select{ |neighbour| active_cubes.include?(neighbour) }.size
+              if active_cubes.include?(candidate)
+                new_active << candidate if num_active_neighbours == 2 || num_active_neighbours == 3 
+              else
+                new_active << candidate if num_active_neighbours == 3
+              end
+            }
+          active_cubes = new_active
+        end
+
+        active_cubes.size
+      end
+
+      def self.neighbours(coord)
+        NeighbourDeltas.map { |delta| [coord[0] + delta[0], coord[1] + delta[1], coord[2] + delta[2]] }
+      end
+
+      def self.hypercube_neighbours(coord)
+        NeighbourDeltas.map { |delta| [coord[0] + delta[0], coord[1] + delta[1], coord[2] + delta[2]] }
+          .map { |neighbour_3d|
+            (-1..1).map { |delta_w|
+              [neighbour_3d[0], neighbour_3d[1], neighbour_3d[2], coord[3] + delta_w]
+            }
+          }.flatten(1) + [[coord[0], coord[1], coord[2], coord[3] - 1], [coord[0], coord[1], coord[2], coord[3] + 1]]
       end
     end
   end
--- spec/day_17_spec.rb
+++ spec/day_17_spec.rb
@@ -3,19 +3,19 @@ require "day_17"
 Day17 = Arihunta::AdventOfCode2020::Day17
 
 describe Day17 do
-  # it "01-00" do
-  #   expect(Day17._01("data/17-00")).to eql(295)
-  # end
+  it "01-00" do
+    expect(Day17._01("data/17-00")).to eql(112)
+  end
 
-  # it "01" do
-  #   expect(Day17._01("data/17")).to eql(102)
-  # end
+  it "01" do
+    expect(Day17._01("data/17")).to eql(237)
+  end
 
-  # it "02-00" do
-  #   expect(Day17._02("data/17-00")).to eql(1068781)
-  # end
+  it "02-00" do
+    expect(Day17._02("data/17-00")).to eql(848)
+  end
 
-  # it "02" do
-  #   expect(Day17._02("data/17")).to eql(42017)
-  # end
+  it "02" do
+    expect(Day17._02("data/17")).to eql(2448)
+  end
 end
